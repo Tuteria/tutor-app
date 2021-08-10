b777c6d5e7c6c93ec382c043af5fb78fc5150ddd
Malik Mahmud <almalikmahmud@gmail.com>
2021-08-10 09:32:17 +0100

    refactor: lazy load components in application index

--- pages/application/index.tsx
+++ pages/application/index.tsx
@@ -1,10 +1,44 @@
 import React from "react";
+import Dynamic from "next/dynamic";
 import TutorPageWrapper from "@tuteria/shared-lib/src/tutor-revamp";
 import { getRootStore } from "@tuteria/shared-lib/src/data/store";
 
+const PersonalInfo = Dynamic(
+  () => import("@tuteria/shared-lib/src/tutor-revamp/PersonalInfo")
+);
+const LocationInfo = Dynamic(
+  () => import("@tuteria/shared-lib/src/tutor-revamp/LocationInfo")
+);
+const WorkHistory = Dynamic(
+ () => import("@tuteria/shared-lib/src/tutor-revamp/WorkHistory")
+);
+const EducationHistory = Dynamic(
+  () => import("@tuteria/shared-lib/src/tutor-revamp/EducationHistory")
+);
+
 const Index = () => {
   const store = getRootStore();
-  return <TutorPageWrapper allCountries={[]} store={store} />;
+  return (
+    <TutorPageWrapper>
+      <PersonalInfo
+        store={store}
+        onSubmit={() => {
+          store.toNextPath();
+        }}
+      />
+
+      <LocationInfo
+        store={store}
+        onSubmit={() => {
+          store.toNextPath();
+        }}
+      />
+
+      <EducationHistory store={store} />
+
+      <WorkHistory store={store} />
+    </TutorPageWrapper>
+  );
 };
 
 export default Index;
