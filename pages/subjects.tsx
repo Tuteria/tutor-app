ca4d1436ea8af8816124c8cdc7c1316559af3766
Biola <gbozee@gmail.com>
2021-12-21 14:31:09 +0100

    save changes

--- pages/subjects.tsx
+++ pages/subjects.tsx
@@ -37,6 +37,7 @@ export default function TutorVerificationPage({
         preferences,
         pricing,
       });
+      debugger;
       store.initializeTutorData({
         ...result,
         tutorInfo: {
