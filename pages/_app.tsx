16b6fdfe0fff5c777daaff16d5d8efaac80e19f0
Biola <gbozee@gmail.com>
2021-12-31 00:25:57 +0100

    added google tag manager to the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,8 +41,27 @@ const MyApp = ({ Component, pageProps }) => {
           />
           <link rel="apple-touch-icon" href="/apple-icon.png"></link>
           <meta name="theme-color" content="#317EFB" />
+          <script dangerouslySetInnerHTML={{__html:`
+            (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:590230,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+            `}}/>
         </Head>
         {/* <DefaultSeo {...seo} /> */}
+          <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NJQC9N"
+    height="0" width="0" style={{"display":"none","visibility":"hidden"}}></iframe></noscript>
+        <script dangerouslySetInnerHTML={{__html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NJQC9N');
+          `}}/>
         <Component {...pageProps} />
       </>
     </ZetaProvider>
