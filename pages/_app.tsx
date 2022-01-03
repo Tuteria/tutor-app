ae778fb798440638e3993d172ed94a527bf060ea
Biola <gbozee@gmail.com>
2022-01-03 08:54:04 +0100

    added additional script for deploying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,20 +25,9 @@ const MyApp = ({ Component, pageProps }) => {
           <meta name="description" content="Description" />
           <meta name="keywords" content="Keywords" />
           <title>Tuteria Tutor Application</title>
-
+          <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <link
-            href="/icons/favicon-16x16.png"
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-          />
-          <link
-            href="/icons/favicon-32x32.png"
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-          />
+         
           <link rel="apple-touch-icon" href="/apple-icon.png"></link>
           <meta name="theme-color" content="#317EFB" />
           <script dangerouslySetInnerHTML={{__html:`
