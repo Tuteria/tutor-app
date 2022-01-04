84318576d967f13040920ddad08cd3f3cb273259
Biola <gbozee@gmail.com>
2022-01-04 19:41:14 +0100

    use axios instead

--- server_utils/cloudinary.ts
+++ server_utils/cloudinary.ts
@@ -1,6 +1,8 @@
 import XFormDAta from "form-data";
 import fs from "fs";
+import http from 'http'
 import fetch from "node-fetch";
+import axios from 'axios'
 import { getCloudinaryDetails } from "@tuteria/tuteria-data/src";
 // const MEDIA_SERVICE = process.env.MEDIA_SERVICE || "http://localhost:8000";
 // const MEDIA_SERVICE = process.env.MEDIA_SERVICE || "http://dev.tuteria.com:8020";
@@ -81,22 +83,60 @@ export async function upload(
   }
   throw "Error saving to cloudinary";
 }
+// export async function upload(
+//   filePath: any,
+//   options: any,
+//   transform: boolean,
+//   quality_check = false,
+//   face_check = false
+// ) {
+//   let new_options = { ...options, file_name: options.public_id };
+//   delete new_options.public_id;
+//   const serverConfig = await getCloudinaryDetails(MEDIA_FORMAT);
+//   let checks: any = {};
+//   if (quality_check) {
+//     checks.quality_analysis = 1;
+//   }
+//   if (face_check) {
+//     checks.detection = "google_face_check";
+//   }
+//   if (Object.keys(checks).length > 0) {
+//     checks = JSON.stringify(checks);
+//   } else {
+//     checks = null;
+//   }
+//   let result = await uploadCloudinaryResource(
+//     filePath,
+//     new_options,
+//     options.kind as any,
+//     JSON.stringify(serverConfig),
+//     checks
+//   );
+//   if (result.full_response) {
+//     let r = result.full_response;
 
-// export function destroy(publicId: string, options: UploadApiOptions) {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.destroy(
-//       publicId,
-//       { resource_type: "auto", ...options },
-//       (err, result) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(result);
-//         }
-//       }
-//     );
-//   });
+//     let response: any = {
+//       public_id: r.public_id,
+//       bytes: r.bytes,
+//       url: r.secure_url,
+//     };
+//     if (r.quality_analysis?.focus) {
+//       const quality = r.quality_analysis?.focus >= 0.1;
+//       response.quality = quality;
+//     }
+//     if (r.vision_ai) {
+//       let hasFace = r.vision_ai.faces_detected > 0;
+//       response.has_face = hasFace;
+//     }
+//     // console.log(JSON.stringify(r));
+//     // if (transform) {
+//     //   response.url = await transformImage(r.public_id, serverConfig);
+//     // }
+//     return response;
+//   }
+//   throw "Error saving to cloudinary";
 // }
+
 
 export async function destroy({ id, kind = "image" }) {
   const serverConfig = await getCloudinaryDetails(MEDIA_FORMAT);
@@ -138,51 +178,51 @@ export async function getCloudinaryUrl(
 
 export type UploadTypes = "image" | "video" | "raw";
 
-export async function uploadCloudinaryResource(
-  fileObj: any,
-  options: any,
-  kind: UploadTypes = "image",
-  serverConfig?: string,
-  checks?: any
-) {
-  let formData = {};
-  if (kind === 'video') {
-    return await uploadCloudinaryFormResource(fileObj, options, kind, serverConfig, checks)
-  }
-  Object.keys(options).forEach((key) => {
-    if (options[key]) {
-      formData[key] = options[key];
-    }
-  });
-  let base64File = fs.readFileSync(fileObj.path, 'base64')
-  let newStream = `data:${fileObj.type};base64,${base64File}`
-  formData[kind] = newStream;
-  formData['kind'] = kind;
-  if (serverConfig) {
-    formData["server_config"] = JSON.parse(serverConfig);
-  }
-  if (checks) {
-    formData["checks"] = JSON.parse(checks);
-  }
-  console.log(formData);
-  let response = await fetch(`https://gsheet.vercel.app/media/${MEDIA_FORMAT}/upload`, {
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: { "content-type": "application/json" },
+// export async function uploadCloudinaryResource(
+//   fileObj: any,
+//   options: any,
+//   kind: UploadTypes = "image",
+//   serverConfig?: string,
+//   checks?: any
+// ) {
+//   let formData = {};
+//   if (kind === 'video') {
+//     return await uploadCloudinaryFormResource(fileObj, options, kind, serverConfig, checks)
+//   }
+//   Object.keys(options).forEach((key) => {
+//     if (options[key]) {
+//       formData[key] = options[key];
+//     }
+//   });
+//   let base64File = fs.readFileSync(fileObj.path, 'base64')
+//   let newStream = `data:${fileObj.type};base64,${base64File}`
+//   formData[kind] = newStream;
+//   formData['kind'] = kind;
+//   if (serverConfig) {
+//     formData["server_config"] = JSON.parse(serverConfig);
+//   }
+//   if (checks) {
+//     formData["checks"] = JSON.parse(checks);
+//   }
+//   console.log(formData);
+//   let response = await fetch(`https://gsheet.vercel.app/media/${MEDIA_FORMAT}/upload`, {
+//     method: "POST",
+//     body: JSON.stringify(formData),
+//     headers: { "content-type": "application/json" },
 
-  });
-  if (response.ok) {
-    let result = await response.json();
-    if (response.status < 400) {
-      return result.data;
-    }
-    return result;
-  }
-  console.log(response)
-  console.log(fileObj.type);
-  throw "Error from server";
-}
-export async function uploadCloudinaryFormResource(
+//   });
+//   if (response.ok) {
+//     let result = await response.json();
+//     if (response.status < 400) {
+//       return result.data;
+//     }
+//     return result;
+//   }
+//   console.log(response)
+//   console.log(fileObj.type);
+//   throw "Error from server";
+// }
+export function uploadCloudinaryResource(
   fileObj: any,
   options: any,
   kind: UploadTypes = "image",
@@ -208,19 +248,68 @@ export async function uploadCloudinaryFormResource(
     formData.append("checks", checks);
   }
   console.log(options);
-  let response = await fetch(`${MEDIA_SERVICE}/media/${MEDIA_FORMAT}/upload`, {
-    method: "POST",
-    body: formData,
-    headers: formData.getHeaders(),
-  });
-  if (response.ok) {
-    let result = await response.json();
-    if (response.status < 400) {
-      return result.data;
-    }
-    return result;
-  }
-  console.log(response)
-  console.log(fileObj.type);
-  throw "Error from server";
+  return axios.post(`${MEDIA_SERVICE}/media/${MEDIA_FORMAT}/upload`,formData,{
+    headers:{...formData.getHeaders()}
+  }).then(response=>{
+    return response.data.data}).catch(error=>error)
+
+  // let response = await fetch(`${MEDIA_SERVICE}/media/${MEDIA_FORMAT}/upload`, {
+  //   method: "POST",
+  //   body: formData,
+  //   headers: formData.getHeaders(),
+  // });
+  // if (response.ok) {
+  //   let result = await response.json();
+  //   if (response.status < 400) {
+  //     return result.data;
+  //   }
+  //   return result;
+  // }
+  // console.log(response)
+  // console.log(fileObj.type);
+  // throw "Error from server";
 }
+
+// export async function uploadCloudinaryResource(
+//   fileObj: any,
+//   options: any,
+//   kind: UploadTypes = "image",
+//   serverConfig?: string,
+//   checks?: any
+// ) {
+//   let formData = new XFormDAta();
+//   if (kind === 'video' && fileObj.type != "video/webm") {
+//     fileObj.type = "video/webm"
+//   }
+//   Object.keys(options).forEach((key) => {
+//     if (options[key]) {
+//       formData.append(key, options[key]);
+//     }
+//   });
+//   let newStream = fs.createReadStream(fileObj.path)
+//   // formData.append(kind, newStream);
+//   formData.append(kind, fileObj.buffer, fileObj.filename)
+//   formData.append("kind", kind);
+//   if (serverConfig) {
+//     formData.append("server_config", serverConfig);
+//   }
+//   if (checks) {
+//     formData.append("checks", checks);
+//   }
+//   console.log(options);
+//   let response = await fetch(`${MEDIA_SERVICE}/media/${MEDIA_FORMAT}/upload`, {
+//     method: "POST",
+//     body: formData,
+//     headers: formData.getHeaders(),
+//   });
+//   if (response.ok) {
+//     let result = await response.json();
+//     if (response.status < 400) {
+//       return result.data;
+//     }
+//     return result;
+//   }
+//   console.log(response)
+//   console.log(fileObj.type);
+//   throw "Error from server";
+// }
