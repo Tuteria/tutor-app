6bd7b6f7b74872890b80213e2fa01113e658e264
usmanoa <usmanakorede@gmail.com>
2022-01-28 12:08:27 +0100

    save change

--- pages/api/hijack-tutor.ts
+++ pages/api/hijack-tutor.ts
@@ -4,13 +4,13 @@ import { serverAdapter } from "../../server_utils/server";
 export default defaultView(
   async (req, res) => {
     const { email, accessCode } = req.query
-    if (accessCode === "AGENT_ACCESS") {
+    if (accessCode === "TUTOR_SUCCESS_ACCESS") {
       const accessToken = await serverAdapter.hijackTutor(email);
       res.writeHead(302, {
         'Location': `/admin?c=${accessToken}`
       })
     } else {
-      res.status(400).json({ status: false, error: "Request failed" })
+      throw "Request failed";
     }
 
   },
