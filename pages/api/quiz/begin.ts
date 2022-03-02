50fccabe74f0d73215c11dd97696af6eaa8b927b
Biola <gbozee@gmail.com>
2022-03-02 14:27:24 +0100

    bumped tuteria-data version to 0.0.2o

--- pages/api/quiz/begin.ts
+++ pages/api/quiz/begin.ts
@@ -4,14 +4,12 @@ import { serverAdapter } from "../../../server_utils/server";
 export default authCheck(
   async (req, userInfo) => {
     let { payload, subject_data } = req.body;
-    let result = await Promise.all([
-      serverAdapter.startQuiz({
-        email: userInfo.personalInfo.email,
-        subjects: payload.subjects,
-      }),
-      serverAdapter.saveTutorSubjectDetails(subject_data),
-    ]);
-    return result[0];
+    let result = await serverAdapter.startQuiz({
+      email: userInfo.personalInfo.email,
+      subjects: payload.subjects,
+    });
+    await serverAdapter.saveTutorSubjectDetails(subject_data);
+    return result;
   },
   { method: "POST" }
 );
