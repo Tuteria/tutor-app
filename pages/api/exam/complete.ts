e18f9ad3a2c2fb150723a002a9d6f31d5596486a
Biola <gbozee@gmail.com>
2022-03-29 10:17:11 +0100

    save changes

--- pages/api/exam/complete.ts
+++ pages/api/exam/complete.ts
@@ -1,22 +1,26 @@
-import { authCheck } from "../../../middlewares";
+import { authCheck, defaultView } from "../../../middlewares";
 import { serverAdapter } from "../../../server_utils/server";
-import { createOrUpdateSubjectWithFlaggedQuestion } from "@tuteria/tuteria-data/src"
+import { createOrUpdateSubjectWithFlaggedQuestion } from "@tuteria/tuteria-data/src";
 
-export default authCheck(
-  async (req, userInfo) => {
+// export default authCheck(
+// async (req, userInfo) => {
+export default defaultView(
+  async (req) => {
+    let { userInfo, ...rest } = req.body;
     let result = await serverAdapter.completeQuiz({
       email: userInfo.personalInfo.email,
-      ...req.body,
+      ...rest,
+      // ...req.body,
     });
     return result.testsTaken;
   },
   {
-    method: "POST", afterResponse: async (req) => {
+    method: "POST",
+    afterResponse: async (req) => {
       let { flagged = [], ...rest } = req.body;
       for (let u = 0; u < flagged.length; u++) {
         await createOrUpdateSubjectWithFlaggedQuestion(flagged[u]);
       }
-
-    }
+    },
   }
 );
