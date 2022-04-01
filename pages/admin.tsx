89da41b5739c28ab75f0099767cd193c3c61a133
Biola <gbozee@gmail.com>
2022-04-01 09:48:09 +0100

    fixed redirect url

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,22 +3,30 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { clientAdapter, NEW_TUTOR_TOKEN } from "../server_utils/client";
 
-const Admin = () => {
-  const router = useRouter()
-  const { c } = router.query;
+const Admin = ({ c, current_step }) => {
+  const router = useRouter();
+  // const { c, current_step = "apply" } = router.query;
   useEffect(() => {
     if (c) {
-      console.log(router)
-      clientAdapter.storage.set(NEW_TUTOR_TOKEN, c)
-      router.push('/apply')
+      console.log(router);
+      clientAdapter.storage.set(NEW_TUTOR_TOKEN, c);
+      router.push(`/${current_step}?access_token=${c}`);
     }
-  }, [c])
+  }, [c]);
 
   return (
     <div>
       <LoadingState />
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps({ query, res }) {
+  const access_token = query.c || "";
+  const current_step = query.current_step || "apply";
+  return {
+    props: { c: access_token, current_step },
+  };
 }
 
-export default Admin
+export default Admin;
--- pages/api/hijack-tutor.ts
+++ pages/api/hijack-tutor.ts
@@ -3,16 +3,15 @@ import { serverAdapter } from "../../server_utils/server";
 
 export default defaultView(
   async (req, res) => {
-    const { email, accessCode } = req.query
+    const { email, accessCode, current_step } = req.query;
     if (accessCode === "TUTOR_SUCCESS_ACCESS") {
       const accessToken = await serverAdapter.hijackTutor(email);
       res.writeHead(302, {
-        'Location': `/admin?c=${accessToken}`
-      })
+        Location: `/admin?c=${accessToken}&current_step=${current_step}`,
+      });
     } else {
       throw "Request failed";
     }
-
   },
   { method: "GET" }
-)
+);
