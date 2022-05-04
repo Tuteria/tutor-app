b1ad707d44271571d35b43a36dde1f03c5df767b
Biola <gbozee@gmail.com>
2022-05-04 13:18:38 +0100

    added nx files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNx = require("@nrwl/next/plugins/with-nx");
+
 const withTM = require("next-transpile-modules");
 const withImages = require("next-images");
 const withPWA = require("next-pwa");
@@ -25,57 +28,62 @@ let transpileModules = [
   "@tuteria/tuteria-data",
 ];
 
-const config = withImages(
-  withTM({
-    // pwa: {
-    //   dest: "public",
-    //   runtimeCaching,
-    // },
-    transpileModules,
-    loaderFunc: (library) => {
-      library.options.plugins = [
-        require("@babel/plugin-transform-flow-strip-types"),
-        // require("emotion")
-      ];
-      return library;
-    },
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-      config.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: "javascript/auto",
-      });
-      return config;
-    },
-    webpackDevMiddleware(config, options) {
-      config.watchOptions.ignored = config.watchOptions.ignored.filter(
-        (ignore) => !ignore.toString().includes("node_modules")
-      );
-      // Ignore all node modules except those here.
-      config.watchOptions.ignored = [
-        ...config.watchOptions.ignored,
-        /node_modules\/(?!@tuteria\/.+)/,
-        /\@tuteria\/.+\/node_modules/,
-        // /node_modules([\\]+|\/)+(?!@tuteria[\\/]shared-lib)/,
-        // /node_modules([\\]+|\/)+(?!@tuteria[\\/]tuteria-data)/,
-        // /\@tuteria[\\/]shared-lib([\\]+|\/)node_modules/,
-        // /\@tuteria[\\/]tuteria-data([\\]+|\/)node_modules/,
-      ];
-      return config;
-    },
-    assetPrefix: process.env.BASE_PATH || "",
-    publicRuntimeConfig: {
-      basePath: process.env.BASE_PATH || "",
-    },
-    reactStrictMode: true,
-    typescript: {
-      ignoreBuildErrors: DISABLE_TYPESCRIPT_ERRORS,
-    },
-  })
+const config = withNx(
+  withImages(
+    withTM({
+      // pwa: {
+      //   dest: "public",
+      //   runtimeCaching,
+      // },
+      nx: {
+        svgr: false,
+      },
+      transpileModules,
+      loaderFunc: (library) => {
+        library.options.plugins = [
+          require("@babel/plugin-transform-flow-strip-types"),
+          // require("emotion")
+        ];
+        return library;
+      },
+      webpack(config, options) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        });
+        config.module.rules.push({
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
+        });
+        return config;
+      },
+      webpackDevMiddleware(config, options) {
+        config.watchOptions.ignored = config.watchOptions.ignored.filter(
+          (ignore) => !ignore.toString().includes("node_modules")
+        );
+        // Ignore all node modules except those here.
+        config.watchOptions.ignored = [
+          ...config.watchOptions.ignored,
+          /node_modules\/(?!@tuteria\/.+)/,
+          /\@tuteria\/.+\/node_modules/,
+          // /node_modules([\\]+|\/)+(?!@tuteria[\\/]shared-lib)/,
+          // /node_modules([\\]+|\/)+(?!@tuteria[\\/]tuteria-data)/,
+          // /\@tuteria[\\/]shared-lib([\\]+|\/)node_modules/,
+          // /\@tuteria[\\/]tuteria-data([\\]+|\/)node_modules/,
+        ];
+        return config;
+      },
+      assetPrefix: process.env.BASE_PATH || "",
+      publicRuntimeConfig: {
+        basePath: process.env.BASE_PATH || "",
+      },
+      reactStrictMode: true,
+      typescript: {
+        ignoreBuildErrors: DISABLE_TYPESCRIPT_ERRORS,
+      },
+    })
+  )
 );
 
 module.exports = process.env.SKIP_SENTRY_UPLOAD
